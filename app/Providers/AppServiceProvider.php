6de37947f9f2ebd4571b3bc161a710bd6ac552a7
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-03-03 20:03:49 +0700

    feat(database): add snowflake ready to use

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Godruoyi\Snowflake\LaravelSequenceResolver;
+use Godruoyi\Snowflake\Snowflake;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
 
@@ -12,7 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+		$this->app->singleton('snowflake', function ($app) {
+			return (new Snowflake())
+				->setStartTimeStamp(strtotime('2019-10-10')*1000)
+				->setSequenceResolver(new LaravelSequenceResolver($app->get('cache.store')));
+		});
     }
 
     /**
