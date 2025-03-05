7fc94fcbf70934c2ff6caf3eb5c6ae52cb5f107b
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-03-05 20:51:46 +0700

    feat: new custom full_name attribute

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -41,7 +42,7 @@ class User extends Authenticatable
         'email',
         'password',
 		'is_customer',
-		'last_login'
+		'last_login',
     ];
 
     /**
@@ -66,7 +67,15 @@ class User extends Authenticatable
 			'last_login' => 'datetime',
 			'created_at' => 'datetime',
 			'updated_at' => 'datetime',
-			'deleted_at' => 'datetime'
+			'deleted_at' => 'datetime',
         ];
     }
+
+	protected function fullName(): Attribute
+	{
+		return Attribute::make(
+			get: fn() => $this->first_name . ' ' . $this->last_name
+		);
+	}
+
 }
