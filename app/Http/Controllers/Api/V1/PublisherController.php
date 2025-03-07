771d5f156463e28b88ce324d9d8161ce6f17cf23
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-03-07 19:35:04 +0700

    feat: sort functionality for publishers

--- app/Http/Controllers/Api/V1/PublisherController.php
+++ app/Http/Controllers/Api/V1/PublisherController.php
@@ -7,14 +7,19 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\PublisherRequest;
 use App\Http\Resources\V1\PublisherCollection;
 use App\Http\Resources\V1\PublisherResource;
+use App\Http\Sorts\V1\PublisherSort;
 use App\Models\Publisher;
+use Illuminate\Http\Request;
 
 
 class PublisherController extends Controller
 {
-	public function index()
+	public function index(Request $request)
 	{
-		return new PublisherCollection(Publisher::paginate());
+		$publisherSort = new PublisherSort($request);
+		$publishers = $publisherSort->apply(Publisher::query())->paginate();
+
+		return new PublisherCollection($publishers);
 	}
 
 	public function store(PublisherRequest $request)
---
+++ app/Http/Sorts/V1/PublisherSort.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Sorts\V1;
+
+
+class PublisherSort extends Sort
+{
+	protected array $sortableColumns = [
+		'name',
+	];
+}
