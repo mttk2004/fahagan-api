212f450d59fcc440b4e172c5de198bf5e3648a16
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-03-07 21:00:21 +0700

    feat(database): fake discounts data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,8 @@ class DatabaseSeeder extends Seeder
 			PublishersSeeder::class,
 			GenresSeeder::class,
 			BooksSeeder::class,
-			AuthorsSeeder::class
+			AuthorsSeeder::class,
+			DiscountsSeeder::class
 		]);
     }
 }
