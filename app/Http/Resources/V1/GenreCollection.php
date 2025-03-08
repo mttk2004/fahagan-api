a86b59b368b06b412c4832e9eb152f187bf39748
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-03-08 10:58:39 +0700

    feat: new GenreCollection

---
+++ app/Http/Resources/V1/GenreCollection.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+
+/** @see \App\Models\Genre */
+class GenreCollection extends ResourceCollection
+{
+	public function toArray(Request $request): array
+	{
+		return [
+			'data' => $this->collection,
+			'included' => [],
+		];
+	}
+}
