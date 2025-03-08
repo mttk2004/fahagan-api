d7620b11c80f7a4d70c192f6e795713b3f376a4a
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-03-08 10:49:49 +0700

    refactor: change type publishers to publisher in PublisherResource

--- app/Http/Resources/V1/PublisherResource.php
+++ app/Http/Resources/V1/PublisherResource.php
@@ -14,7 +14,7 @@ class PublisherResource extends JsonResource
 	public function toArray(Request $request): array
 	{
 		return [
-			'type' => 'publishers',
+			'type' => 'publisher',
 			'id' => $this->id,
 			'attributes' => [
 				'name' => $this->name,
