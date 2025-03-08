39d3f0e8984756095a8f5e65f28badcde876ad6f
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-03-08 11:03:54 +0700

    refactor: add logic for routes genres.index & genre.show

--- app/Http/Controllers/Api/V1/GenreController.php
+++ app/Http/Controllers/Api/V1/GenreController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api\V1;
 
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\GenreCollection;
+use App\Http\Resources\V1\GenreResource;
 use App\Models\Genre;
 use Illuminate\Http\Request;
 
@@ -12,12 +14,14 @@ class GenreController extends Controller
 {
 	public function index()
 	{
-		
+		return new GenreCollection(Genre::paginate());
 	}
 
 	public function store(Request $request) {}
 
-	public function show(Genre $genre) {}
+	public function show(Genre $genre) {
+		return new GenreResource($genre);
+	}
 
 	public function update(Request $request, Genre $genre) {}
 
