78823700d25a7dc85c7b9f471fd5d872b325d2d7
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-03-10 21:08:58 +0700

    feat: add logic for users.destroy

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -9,12 +9,14 @@ use App\Http\Resources\V1\UserResource;
 use App\Http\Sorts\V1\UserSort;
 use App\Models\User;
 use App\Traits\ApiResponses;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 
 class UserController extends Controller
 {
 	use ApiResponses;
+
 
 	public function index(Request $request)
 	{
@@ -31,5 +33,20 @@ class UserController extends Controller
 
 	public function update(Request $request, User $user) {}
 
-	public function destroy(User $user) {}
+	public function destroy(Request $request, $user_id)
+	{
+		$user = $request->user();
+		if (!$user->hasPermissionTo('delete_users') || $user->id != $user_id) {
+			return $this->error('Bạn không có quyền thực hiện hành động này.', 403);
+		}
+
+		try {
+			$userToDelete = User::findOrFail($user_id);
+			$userToDelete->delete();
+
+			return $this->ok('Xóa người dùng thành công.');
+		} catch (ModelNotFoundException) {
+			return $this->error('Người dùng không tồn tại.', 404);
+		}
+	}
 }
