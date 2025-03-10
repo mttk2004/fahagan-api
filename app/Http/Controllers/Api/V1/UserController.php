14083a8f2d1383d84ec7577fadc5c79501ff6477
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-03-10 21:19:06 +0700

    feat: new logic for users.update

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\UserUpdateRequest;
 use App\Http\Resources\V1\UserCollection;
 use App\Http\Resources\V1\UserResource;
 use App\Http\Sorts\V1\UserSort;
@@ -31,7 +32,18 @@ class UserController extends Controller
 		return new UserResource($user);
 	}
 
-	public function update(Request $request, User $user) {}
+	public function update(UserUpdateRequest $request, $user_id)
+	{
+		try {
+			$user = User::findOrFail($user_id);
+			$userData = $request->validated()['data']['attributes'];
+			$user->update($userData);
+
+			return new UserResource($user);
+		} catch (ModelNotFoundException) {
+			return $this->error('Người dùng không tồn tại.', 404);
+		}
+	}
 
 	public function destroy(Request $request, $user_id)
 	{
--- app/Http/Requests/V1/UserUpdateRequest.php
+++ app/Http/Requests/V1/UserUpdateRequest.php
@@ -3,7 +3,11 @@
 namespace App\Http\Requests\V1;
 
 
+use App\Models\User;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
 
 
 class UserUpdateRequest extends FormRequest
@@ -11,12 +15,64 @@ class UserUpdateRequest extends FormRequest
 	public function rules(): array
 	{
 		return [
-			
+			'data.attributes.first_name' => ['sometimes', 'string', 'max:30'],
+			'data.attributes.last_name' => ['sometimes', 'string', 'max:30'],
+			'data.attributes.phone' => [
+				'sometimes',
+				'string',
+				'regex:/^0[35789][0-9]{8}$/',
+				'unique:' . User::class,
+			],
+			'data.attributes.email' => [
+				'sometimes',
+				'string',
+				'lowercase',
+				'email',
+				'max:50',
+				'unique:' . User::class,
+			],
+			'data.attributes.password' => ['sometimes', 'confirmed', Password::default()],
 		];
 	}
 
-	public function authorize(): bool
+	public function messages(): array
 	{
-		return true;
+		return [
+			'data.attributes.first_name' => [
+				'string' => 'Tên nên là một chuỗi.',
+				'max:30' => 'Tên nên có độ dài tối đa 30.',
+			],
+			'data.attributes.last_name' => [
+				'string' => 'Họ nên là một chuỗi.',
+				'max:30' => 'Họ nên có độ dài tối đa 30.',
+			],
+			'data.attributes.phone' => [
+				'string' => 'Số điện thoại nên là một chuỗi.',
+				'regex' => 'Số điện thoại không hợp lệ.',
+				'unique' => 'Số điện thoại đã được sử dụng.',
+			],
+			'data.attributes.email' => [
+				'string' => 'Email nên là một chuỗi.',
+				'lowercase' => 'Email nên viết thường.',
+				'email' => 'Email không hợp lệ.',
+				'max:50' => 'Email nên có độ dài tối đa 50.',
+				'unique' => 'Email đã được sử dụng.',
+			],
+			'data.attributes.password' => [
+				'confirmed' => 'Mật khẩu không khớp.',
+				'password' => 'Mật khẩu nên chứa ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường, số và ký tự đặc biệt.',
+			],
+		];
+	}
+
+	public function authorize(Request $request): bool
+	{
+		return $request->user()->hasPermissionTo('edit_users')
+			|| $request->user()->id == $request->route('user');
+	}
+
+	public function failedAuthorization()
+	{
+		throw new AuthorizationException('Bạn không có quyền thực hiện hành động này.');
 	}
 }
