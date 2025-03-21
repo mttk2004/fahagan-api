67c40e00eefa1ccb9b543d465c7754bfc7339c12
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-03-21 16:27:28 +0700

    refactor(form-request): update rules for PublisherUpdateRequest

--- app/Http/Requests/V1/PublisherUpdateRequest.php
+++ app/Http/Requests/V1/PublisherUpdateRequest.php
@@ -12,7 +12,7 @@ class PublisherUpdateRequest extends BaseRequest
 	public function rules(): array
 	{
 		return [
-			'data.attributes.name' => ['sometimes', 'string'],
+			'data.attributes.name' => ['sometimes', 'string', 'max:255', 'unique:publishers,name'],
 			'data.attributes.biography' => ['sometimes', 'string'],
 		];
 	}
@@ -21,6 +21,8 @@ class PublisherUpdateRequest extends BaseRequest
 	{
 		return [
 			'data.attributes.name.string' => 'Tên nhà xuất bản nên là một chuỗi.',
+			'data.attributes.name.max' => 'Tên nhà xuất bản nên có độ dài tối đa 255.',
+			'data.attributes.name.unique' => 'Tên nhà xuất bản đã tồn tại.',
 			'data.attributes.biography.string' => 'Tiểu sử nhà xuất bản nên là một chuỗi.',
 		];
 	}
