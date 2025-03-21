c2594ae3e2c35b3c93e70ed6acbd63f5952dd10a
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-03-21 16:24:18 +0700

    refactor(form-request): update rules for GenreStoreRequest

--- app/Http/Requests/V1/GenreStoreRequest.php
+++ app/Http/Requests/V1/GenreStoreRequest.php
@@ -12,7 +12,7 @@ class GenreStoreRequest extends BaseRequest
 	public function rules(): array
 	{
 		return [
-			'data.attributes.name' => ['required', 'string'],
+			'data.attributes.name' => ['required', 'string', 'max:255', 'unique:genres,name'],
 			'data.attributes.description' => ['required', 'string'],
 		];
 	}
@@ -22,6 +22,8 @@ class GenreStoreRequest extends BaseRequest
 		return [
 			'data.attributes.name.required' => 'Tên thể loại là trường bắt buộc.',
 			'data.attributes.name.string' => 'Tên thể loại nên là một chuỗi.',
+			'data.attributes.name.max' => 'Tên thể loại nên có độ dài tối đa 255.',
+			'data.attributes.name.unique' => 'Tên thể loại đã tồn tại.',
 			'data.attributes.description.required' => 'Mô tả thể loại là trường bắt buộc.',
 			'data.attributes.description.string' => 'Mô tả thể loại nên là một chuỗi.',
 		];
