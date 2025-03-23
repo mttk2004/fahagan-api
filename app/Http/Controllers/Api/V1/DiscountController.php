c95a64a4e4c428174ad2791204623f85a683dba6
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-03-23 14:04:58 +0700

    feat(controller): new logic for discounts.update

--- app/Http/Controllers/Api/V1/DiscountController.php
+++ app/Http/Controllers/Api/V1/DiscountController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Enums\ResponseMessage;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\DiscountStoreRequest;
+use App\Http\Requests\V1\DiscountUpdateRequest;
 use App\Http\Resources\V1\DiscountCollection;
 use App\Http\Resources\V1\DiscountResource;
 use App\Http\Sorts\V1\DiscountSort;
@@ -14,14 +15,32 @@ use App\Traits\ApiResponses;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-
-use function PHPUnit\Framework\throwException;
+use Illuminate\Support\Collection;
 
 
 class DiscountController extends Controller
 {
 	use ApiResponses;
 
+	/**
+	 * Validate and map targets
+	 *
+	 * @param array $targetsData
+	 *
+	 * @return Collection|JsonResponse
+	 */
+	private function validateAndMapTargets(array $targetsData)
+	{
+		try {
+			return collect($targetsData)->map(function($target) {
+				$targetType = 'App\Models\\' . ucfirst($target['type']);
+				$targetType::findOrFail($target['id']);
+				return ['target_type' => $targetType, 'target_id' => $target['id']];
+			});
+		} catch (ModelNotFoundException) {
+			return $this->notFound(ResponseMessage::NOT_FOUND_TARGET_OBJECT->value);
+		}
+	}
 
 	/**
 	 * Get all discounts
@@ -57,15 +76,9 @@ class DiscountController extends Controller
 		$validatedData = $request->validated();
 		$targetsData = $validatedData['data']['relationships']['targets'];
 
-		try {
-			$targets = collect($targetsData)->map(function($target) {
-				$targetType = 'App\Models\\' . ucfirst($target['type']);
-				$targetType::findOrFail($target['id']);
-
-				return ['target_type' => $targetType, 'target_id' => $target['id']];
-			});
-		} catch (ModelNotFoundException) {
-			return $this->notFound(ResponseMessage::NOT_FOUND_TARGET_OBJECT->value);
+		$targets = $this->validateAndMapTargets($targetsData);
+		if ($targets instanceof JsonResponse) {
+			return $targets;
 		}
 
 		$discount = Discount::create($validatedData['data']['attributes']);
@@ -102,13 +115,40 @@ class DiscountController extends Controller
 	/**
 	 * Update a discount
 	 *
-	 * @param Request  $request
-	 * @param Discount $discount
+	 * @param DiscountUpdateRequest $request
+	 * @param                       $discount_id
 	 *
-	 * @return void
+	 * @return JsonResponse
 	 * @group Discounts
 	 */
-	public function update(Request $request, Discount $discount) {}
+	public function update(DiscountUpdateRequest $request, $discount_id)
+	{
+		try {
+			$discount = Discount::findOrFail($discount_id);
+			$validatedData = $request->validated();
+			$discountData = $validatedData['data']['attributes'];
+
+			if (isset($validatedData['data']['relationships']['targets'])) {
+				$targetsData = $validatedData['data']['relationships']['targets'];
+
+				$targets = $this->validateAndMapTargets($targetsData);
+				if ($targets instanceof JsonResponse) {
+					return $targets;
+				}
+
+				$discount->targets()->delete();
+				$discount->targets()->createMany($targets);
+			}
+
+			$discount->update($discountData);
+
+			return $this->ok(ResponseMessage::UPDATED_DISCOUNT->value, [
+				'discount' => new DiscountResource($discount),
+			]);
+		} catch (ModelNotFoundException) {
+			return $this->notFound(ResponseMessage::NOT_FOUND_DISCOUNT->value);
+		}
+	}
 
 	/**
 	 * Delete a discount
--- app/Http/Requests/V1/DiscountUpdateRequest.php
+++ app/Http/Requests/V1/DiscountUpdateRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests\V1;
 
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
 
 
 class DiscountUpdateRequest extends FormRequest
@@ -11,12 +12,45 @@ class DiscountUpdateRequest extends FormRequest
 	public function rules(): array
 	{
 		return [
-			
+			'data.attributes.name' => ['sometimes', 'string', 'max:255', 'unique:discounts,name'],
+			'data.attributes.discount_type' => ['sometimes', 'string', 'in:percent,fixed'],
+			'data.attributes.discount_value' => ['sometimes', 'decimal:', 'min:0'],
+			'data.attributes.start_date' => ['sometimes', 'date', 'after:today'],
+			'data.attributes.end_date' => ['sometimes', 'date', 'after:data.attributes.start_date'],
+
+			'data.relationships.targets' => ['sometimes', 'array'],
+			'data.relationships.targets.*.type' => [
+				'sometimes',
+				'string',
+				'in:book,author,publisher,genre',
+			],
+			'data.relationships.targets.*.id' => ['sometimes', 'integer'],
 		];
 	}
 
-	public function authorize(): bool
+	public function messages(): array
 	{
-		return true;
+		return [
+			'data.attributes.name.string' => 'Tên mã giảm giá nên là một chuỗi.',
+			'data.attributes.name.max' => 'Tên mã giảm giá nên có độ dài tối đa 255.',
+			'data.attributes.name.unique' => 'Tên mã giảm giá đã tồn tại.',
+			'data.attributes.discount_type.string' => 'Loại giảm giá nên là một chuỗi.',
+			'data.attributes.discount_type.in' => 'Loại giảm giá không hợp lệ.',
+			'data.attributes.discount_value.decimal' => 'Giá trị giảm giá nên là một số.',
+			'data.attributes.discount_value.min' => 'Giá trị giảm giá không hợp lệ.',
+			'data.attributes.start_date.date' => 'Ngày bắt đầu không hợp lệ.',
+			'data.attributes.start_date.after' => 'Ngày bắt đầu phải sau ngày hiện tại.',
+			'data.attributes.end_date.date' => 'Ngày kết thúc không hợp lệ.',
+			'data.attributes.end_date.after' => 'Ngày kết thúc phải sau ngày bắt đầu.',
+			'data.relationships.targets.array' => 'Đối tượng áp dụng nên là một mảng.',
+			'data.relationships.targets.*.type.string' => 'Loại đối tượng áp dụng nên là một chuỗi.',
+			'data.relationships.targets.*.type.in' => 'Loại đối tượng áp dụng không hợp lệ.',
+			'data.relationships.targets.*.id.integer' => 'ID đối tượng áp dụng nên là một số.',
+		];
+	}
+
+	public function authorize(Request $request): bool
+	{
+		return $request->user()->hasPermissionTo('edit_discounts');
 	}
 }
