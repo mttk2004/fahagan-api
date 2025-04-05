48fb96f83845ed6057dea1fca637a09ff23968bf
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-04-05 15:04:55 +0700

    refactor: update rules for login & register

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -6,7 +6,6 @@ use App\Http\Requests\BaseRequest;
 use App\Interfaces\HasValidationMessages;
 use App\Traits\HasRequestFormat;
 use App\Utils\AuthUtils;
-use Illuminate\Validation\Rules\Password;
 
 class LoginRequest extends BaseRequest implements HasValidationMessages
 {
@@ -20,31 +19,31 @@ class LoginRequest extends BaseRequest implements HasValidationMessages
     public function rules(): array
     {
         return [
-            'data.attributes.email' => [
-                'required',
-                'string',
-                'email',
-                'max:50',
-            ],
-            'data.attributes.password' => [
-                'required',
-                'string',
-                Password::default(),
-            ],
+          'data.attributes.email' => [
+            'required',
+            'string',
+            'email',
+            'max:50',
+          ],
+          'data.attributes.password' => [
+            'required',
+            'string',
+            'min:8',
+          ],
         ];
     }
 
     public function messages(): array
     {
         return [
-            'data.attributes.email.required' => 'Email là trường bắt buộc.',
-            'data.attributes.email.string' => 'Email nên là một chuỗi.',
-            'data.attributes.email.email' => 'Email không hợp lệ.',
-            'data.attributes.email.max' => 'Email nên có độ dài tối đa 50.',
+          'data.attributes.email.required' => 'Email là trường bắt buộc.',
+          'data.attributes.email.string' => 'Email nên là một chuỗi.',
+          'data.attributes.email.email' => 'Email không hợp lệ.',
+          'data.attributes.email.max' => 'Email nên có độ dài tối đa 50.',
 
-            'data.attributes.password.required' => 'Mật khẩu là trường bắt buộc.',
-            'data.attributes.password.string' => 'Mật khẩu nên là một chuỗi.',
-            'data.attributes.password.password' => 'Mật khẩu nên chứa ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường, số và ký tự đặc biệt.',
+          'data.attributes.password.required' => 'Mật khẩu là trường bắt buộc.',
+          'data.attributes.password.string' => 'Mật khẩu nên là một chuỗi.',
+          'data.attributes.password.min' => 'Mật khẩu nên có ít nhất 8 ký tự.',
         ];
     }
 
--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -6,7 +6,6 @@ use App\Http\Requests\BaseRequest;
 use App\Interfaces\HasValidationMessages;
 use App\Traits\HasRequestFormat;
 use App\Utils\AuthUtils;
-use Illuminate\Validation\Rules\Password;
 
 class RegisterRequest extends BaseRequest implements HasValidationMessages
 {
@@ -20,56 +19,56 @@ class RegisterRequest extends BaseRequest implements HasValidationMessages
     public function rules(): array
     {
         return [
-            'data.attributes.first_name' => ['required', 'string', 'max:30'],
-            'data.attributes.last_name' => ['required', 'string', 'max:30'],
-            'data.attributes.phone' => [
-                'required',
-                'string',
-                'regex:/^0[35789][0-9]{8}$/',
-                'unique:users,phone',
-            ],
-            'data.attributes.email' => [
-                'required',
-                'string',
-                'email',
-                'max:50',
-                'unique:users,email',
-            ],
-            'data.attributes.password' => [
-                'required',
-                'string',
-                'confirmed',
-                Password::default(),
-            ],
+          'data.attributes.first_name' => ['required', 'string', 'max:30'],
+          'data.attributes.last_name' => ['required', 'string', 'max:30'],
+          'data.attributes.phone' => [
+            'required',
+            'string',
+            'regex:/^0[35789][0-9]{8}$/',
+            'unique:users,phone',
+          ],
+          'data.attributes.email' => [
+            'required',
+            'string',
+            'email',
+            'max:50',
+            'unique:users,email',
+          ],
+          'data.attributes.password' => [
+            'required',
+            'string',
+            'min:8',
+            'confirmed',
+          ],
         ];
     }
 
     public function messages(): array
     {
         return [
-            'data.attributes.first_name.required' => 'Tên là trường bắt buộc.',
-            'data.attributes.first_name.string' => 'Tên nên là một chuỗi.',
-            'data.attributes.first_name.max' => 'Tên nên có độ dài tối đa 30.',
+          'data.attributes.first_name.required' => 'Tên là trường bắt buộc.',
+          'data.attributes.first_name.string' => 'Tên nên là một chuỗi.',
+          'data.attributes.first_name.max' => 'Tên nên có độ dài tối đa 30.',
 
-            'data.attributes.last_name.required' => 'Họ là trường bắt buộc.',
-            'data.attributes.last_name.string' => 'Họ nên là một chuỗi.',
-            'data.attributes.last_name.max' => 'Họ nên có độ dài tối đa 30.',
+          'data.attributes.last_name.required' => 'Họ là trường bắt buộc.',
+          'data.attributes.last_name.string' => 'Họ nên là một chuỗi.',
+          'data.attributes.last_name.max' => 'Họ nên có độ dài tối đa 30.',
 
-            'data.attributes.phone.required' => 'Số điện thoại là trường bắt buộc.',
-            'data.attributes.phone.string' => 'Số điện thoại nên là một chuỗi.',
-            'data.attributes.phone.regex' => 'Số điện thoại không hợp lệ.',
-            'data.attributes.phone.unique' => 'Số điện thoại đã được sử dụng.',
+          'data.attributes.phone.required' => 'Số điện thoại là trường bắt buộc.',
+          'data.attributes.phone.string' => 'Số điện thoại nên là một chuỗi.',
+          'data.attributes.phone.regex' => 'Số điện thoại không hợp lệ.',
+          'data.attributes.phone.unique' => 'Số điện thoại đã được sử dụng.',
 
-            'data.attributes.email.required' => 'Email là trường bắt buộc.',
-            'data.attributes.email.string' => 'Email nên là một chuỗi.',
-            'data.attributes.email.email' => 'Email không hợp lệ.',
-            'data.attributes.email.max' => 'Email nên có độ dài tối đa 50.',
-            'data.attributes.email.unique' => 'Email đã được sử dụng.',
+          'data.attributes.email.required' => 'Email là trường bắt buộc.',
+          'data.attributes.email.string' => 'Email nên là một chuỗi.',
+          'data.attributes.email.email' => 'Email không hợp lệ.',
+          'data.attributes.email.max' => 'Email nên có độ dài tối đa 50.',
+          'data.attributes.email.unique' => 'Email đã được sử dụng.',
 
-            'data.attributes.password.required' => 'Mật khẩu là trường bắt buộc.',
-            'data.attributes.password.string' => 'Mật khẩu nên là một chuỗi.',
-            'data.attributes.password.confirmed' => 'Mật khẩu không khớp.',
-            'data.attributes.password.password' => 'Mật khẩu nên chứa ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường, số và ký tự đặc biệt.',
+          'data.attributes.password.required' => 'Mật khẩu là trường bắt buộc.',
+          'data.attributes.password.string' => 'Mật khẩu nên là một chuỗi.',
+          'data.attributes.password.min' => 'Mật khẩu nên có ít nhất 8 ký tự.',
+          'data.attributes.password.confirmed' => 'Mật khẩu xác nhận không khớp.',
         ];
     }
 
