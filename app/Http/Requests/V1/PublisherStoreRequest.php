672843b5ac6a94cdf25256de9f41790628583c3f
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-04-05 10:27:17 +0700

    fix(test): test_it_can_restore_soft_deleted_publisher

--- app/Http/Requests/V1/PublisherStoreRequest.php
+++ app/Http/Requests/V1/PublisherStoreRequest.php
@@ -9,36 +9,36 @@ use App\Utils\AuthUtils;
 
 class PublisherStoreRequest extends BaseRequest implements HasValidationMessages
 {
-    use HasRequestFormat;
+  use HasRequestFormat;
 
-    protected function prepareForValidation(): void
-    {
-        $this->convertToJsonApiFormat(['name', 'biography']);
-    }
+  protected function prepareForValidation(): void
+  {
+    $this->convertToJsonApiFormat(['name', 'biography']);
+  }
 
-    public function rules(): array
-    {
-        return [
-            'data.attributes.name' => ['required', 'string', 'max:255', 'unique:publishers,name'],
-            'data.attributes.biography' => ['required', 'string'],
-        ];
-    }
+  public function rules(): array
+  {
+    return [
+      'data.attributes.name' => ['required', 'string', 'max:255', 'unique:publishers,name,NULL,id,deleted_at,NULL'],
+      'data.attributes.biography' => ['required', 'string'],
+    ];
+  }
 
-    public function messages(): array
-    {
-        return [
-            'data.attributes.name.required' => 'Tên nhà xuất bản là trường bắt buộc.',
-            'data.attributes.name.string' => 'Tên nhà xuất bản nên là một chuỗi.',
-            'data.attributes.name.max' => 'Tên nhà xuất bản nên có độ dài tối đa 255.',
-            'data.attributes.name.unique' => 'Tên nhà xuất bản đã tồn tại.',
+  public function messages(): array
+  {
+    return [
+      'data.attributes.name.required' => 'Tên nhà xuất bản là trường bắt buộc.',
+      'data.attributes.name.string' => 'Tên nhà xuất bản nên là một chuỗi.',
+      'data.attributes.name.max' => 'Tên nhà xuất bản nên có độ dài tối đa 255.',
+      'data.attributes.name.unique' => 'Tên nhà xuất bản đã tồn tại.',
 
-            'data.attributes.biography.required' => 'Tiểu sử nhà xuất bản là trường bắt buộc.',
-            'data.attributes.biography.string' => 'Tiểu sử nhà xuất bản nên là một chuỗi.',
-        ];
-    }
+      'data.attributes.biography.required' => 'Tiểu sử nhà xuất bản là trường bắt buộc.',
+      'data.attributes.biography.string' => 'Tiểu sử nhà xuất bản nên là một chuỗi.',
+    ];
+  }
 
-    public function authorize(): bool
-    {
-        return AuthUtils::userCan('create_publishers');
-    }
+  public function authorize(): bool
+  {
+    return AuthUtils::userCan('create_publishers');
+  }
 }
