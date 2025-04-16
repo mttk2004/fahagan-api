232ac45572a7d97dcfc3bcf514389a08dadd06fe
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-04-16 11:10:01 +0700

    style: format api_v1 routes

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -15,10 +15,10 @@ use Illuminate\Support\Facades\Route;
  * Unauthenticated area
  */
 Route::apiResources([
-    'books' => BookController::class,
-    'authors' => AuthorController::class,
-    'publishers' => PublisherController::class,
-    'genres' => GenreController::class,
+  'books' => BookController::class,
+  'authors' => AuthorController::class,
+  'publishers' => PublisherController::class,
+  'genres' => GenreController::class,
 ], ['only' => ['index', 'show']]);
 
 /**
@@ -29,7 +29,7 @@ Route::middleware('auth.*')->group(function () {
      * Both customer and employee can access
      */
     Route::apiResource('users', UserController::class)
-         ->except('store');
+      ->except('store');
 
     /**
      * Customer only area
@@ -37,27 +37,21 @@ Route::middleware('auth.*')->group(function () {
     Route::middleware('auth.customer')->group(function () {
         // Cart
         Route::get('cart', [CartItemController::class, 'index'])
-             ->name('cart.index');
+          ->name('cart.index');
         Route::post('cart/add', [CartItemController::class, 'addToCart'])
-             ->name('cart.add');
+          ->name('cart.add');
         Route::post('cart/update-quantity', [CartItemController::class, 'updateCartItemQuantity'])
-             ->name('cart.update-quantity');
+          ->name('cart.update-quantity');
         Route::delete(
             'cart/remove/{book_id}',
             [CartItemController::class, 'removeFromCart']
         )
-             ->whereNumber('book_id')
-             ->name('cart.remove');
+          ->whereNumber('book_id')
+          ->name('cart.remove');
 
         // Addresses
-        Route::get('addresses', [AddressController::class, 'index'])
-             ->name('addresses.index');
-        Route::post('addresses', [AddressController::class, 'store'])
-             ->name('addresses.store');
-        Route::patch('addresses/{address_id}', [AddressController::class, 'update'])
-             ->name('addresses.update');
-        Route::delete('addresses/{address_id}', [AddressController::class, 'destroy'])
-             ->name('addresses.destroy');
+        Route::apiResource('addresses', AddressController::class)
+          ->except('show');
     });
 
     /**
@@ -65,35 +59,35 @@ Route::middleware('auth.*')->group(function () {
      */
     Route::middleware('auth.employee')->group(function () {
         Route::apiResources([
-            'books' => BookController::class,
-            'authors' => AuthorController::class,
-            'publishers' => PublisherController::class,
-            'genres' => GenreController::class,
+          'books' => BookController::class,
+          'authors' => AuthorController::class,
+          'publishers' => PublisherController::class,
+          'genres' => GenreController::class,
         ], ['except' => ['index', 'show']]);
 
         Route::apiResources([
-            'discounts' => DiscountController::class,
-            'suppliers' => SupplierController::class,
+          'discounts' => DiscountController::class,
+          'suppliers' => SupplierController::class,
         ]);
     });
 });
 
 // Genres routes
 Route::middleware('auth.*')
-    ->prefix('genres')
-    ->group(function () {
-        Route::post('/restore/{genre}', [GenreController::class, 'restore'])
-            ->name('genres.restore');
-    });
+  ->prefix('genres')
+  ->group(function () {
+      Route::post('/restore/{genre}', [GenreController::class, 'restore'])
+        ->name('genres.restore');
+  });
 
 Route::apiResource('genres', GenreController::class);
 Route::get('genres/slug/{slug}', [GenreController::class, 'showBySlug'])
-    ->name('genres.showBySlug');
+  ->name('genres.showBySlug');
 
 // Suppliers routes
 Route::middleware('auth.*')
-    ->prefix('suppliers')
-    ->group(function () {
-        Route::post('/restore/{supplier}', [SupplierController::class, 'restore'])
-            ->name('suppliers.restore');
-    });
+  ->prefix('suppliers')
+  ->group(function () {
+      Route::post('/restore/{supplier}', [SupplierController::class, 'restore'])
+        ->name('suppliers.restore');
+  });
