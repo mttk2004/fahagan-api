9bef61ce8893fd8ec049aa88a043eb85bc56cb1d
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-05-09 09:12:14 +0700

    refactor(traits): allow max_per_page upto 10000

--- app/Traits/HandlePagination.php
+++ app/Traits/HandlePagination.php
@@ -6,14 +6,14 @@ use Illuminate\Http\Request;
 
 trait HandlePagination
 {
-    protected function getPerPage(Request $request, int $default = 20, int $maxPerPage = 60): int
-    {
-        $perPage = (int) $request->get('per_page', $default);
+  protected function getPerPage(Request $request, int $default = 20, int $maxPerPage = 10000): int
+  {
+    $perPage = (int) $request->get('per_page', $default);
 
-        if ($perPage < 10) {
-            return $default;
-        }
-
-        return min($perPage, $maxPerPage);
+    if ($perPage < 10) {
+      return $default;
     }
+
+    return min($perPage, $maxPerPage);
+  }
 }
