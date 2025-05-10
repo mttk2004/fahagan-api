d81afabefbc6b610f1122d192fcd18cdd270d13e
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-05-10 19:06:30 +0700

    feat(discounts): allow to filter by active status

--- app/Http/Filters/V1/DiscountFilter.php
+++ app/Http/Filters/V1/DiscountFilter.php
@@ -7,66 +7,67 @@ use Illuminate\Http\Request;
 
 class DiscountFilter
 {
-    protected Request $request;
+  protected Request $request;
 
-    protected array $filters;
+  protected array $filters;
 
-    public function __construct(Request $request)
-    {
-        $this->request = $request;
-        $this->filters = $request->get('filter', []);
+  public function __construct(Request $request)
+  {
+    $this->request = $request;
+    $this->filters = $request->get('filter', []);
+  }
+
+  public function apply(Builder $query): Builder
+  {
+    if (empty($this->filters)) {
+      return $query;
     }
 
-    public function apply(Builder $query): Builder
-    {
-        if (empty($this->filters)) {
-            return $query;
-        }
+    $this->filterByName($query);
+    $this->filterByType($query);
+    $this->filterByValue($query);
+    $this->filterByDate($query);
+    $this->filterByActive($query);
 
-        $this->filterByName($query);
-        $this->filterByType($query);
-        $this->filterByValue($query);
-        $this->filterByDate($query);
+    return $query;
+  }
 
-        return $query;
+  protected function filterByName(Builder $query): void
+  {
+    if (isset($this->filters['name'])) {
+      $query->where('name', 'like', '%' . $this->filters['name'] . '%');
+    }
+  }
+
+  protected function filterByType(Builder $query): void
+  {
+    if (isset($this->filters['discount_type'])) {
+      $query->where('discount_type', $this->filters['discount_type']);
+    }
+  }
+
+  protected function filterByValue(Builder $query): void
+  {
+    if (isset($this->filters['discount_value'])) {
+      $query->where('discount_value', '>=', $this->filters['discount_value']);
+    }
+  }
+
+  protected function filterByDate(Builder $query): void
+  {
+    if (isset($this->filters['start_date'])) {
+      $query->where('start_date', '>=', $this->filters['start_date']);
     }
 
-    protected function filterByName(Builder $query): void
-    {
-        if (isset($this->filters['name'])) {
-            $query->where('name', 'like', '%' . $this->filters['name'] . '%');
-        }
+    if (isset($this->filters['end_date'])) {
+      $query->where('end_date', '<=', $this->filters['end_date']);
     }
+  }
 
-    protected function filterByType(Builder $query): void
-    {
-        if (isset($this->filters['discount_type'])) {
-            $query->where('discount_type', $this->filters['discount_type']);
-        }
+  protected function filterByActive(Builder $query): void
+  {
+    if (isset($this->filters['is_active'])) {
+      $query->where('is_active', $this->filters['is_active']);
     }
-
-    protected function filterByValue(Builder $query): void
-    {
-        if (isset($this->filters['discount_value'])) {
-            $query->where('discount_value', '>=', $this->filters['discount_value']);
-        }
-    }
-
-    protected function filterByDate(Builder $query): void
-    {
-        if (isset($this->filters['start_date'])) {
-            $query->where('start_date', '>=', $this->filters['start_date']);
-        }
-
-        if (isset($this->filters['end_date'])) {
-            $query->where('end_date', '<=', $this->filters['end_date']);
-        }
-    }
-
-    protected function filterByActive(Builder $query): void
-    {
-        if (isset($this->filters['is_active'])) {
-            $query->where('is_active', $this->filters['is_active']);
-        }
-    }
+  }
 }
